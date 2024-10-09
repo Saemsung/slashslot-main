c5b570b7aad2f5b09c8e5ae0190cc8a4f13d263f
Saemsung <roberto12sam@gmail.com>
2024-10-09 15:43:27 +0200

    Il tuo messaggio di commit qui

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,43 +1,71 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  mode: isProduction ? 'production' : 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: isProduction ? '[name].[contenthash].js' : 'bundle.js',
     publicPath: '/',
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico',
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: 'public', to: 'public' }],
+      patterns: [
+        { 
+          from: 'public', 
+          to: 'public',
+          globOptions: {
+            ignore: ['**/index.html', '**/favicon.ico'],
+          },
+        }
+      ],
     }),
+    ...(isProduction ? [new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    })] : []),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // Aggiungi questa linea per l'alias
+      '@': path.resolve(__dirname, 'src'),
     },
-    extensions: ['.js', '.json'], // Opzionale: permette di omettere le estensioni durante l'import
+    extensions: ['.js', '.jsx', '.json'],
   },
   devServer: {
     static: {
@@ -55,4 +83,11 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
 };
